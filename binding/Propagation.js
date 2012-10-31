472949d0c988675138f5b3dd7d693f533cbb9dfb
Takuya Ueda <t.ueda@netcom-inc.co.jp>
2012-10-31 18:00:27 +0900

    change namespace

---
+++ binding/Propagation.js
@@ -0,0 +1,5 @@
+/**
+ * It provide context of propagation and can test availability of propagation.
+ * @typedef {function(sb.binding.ObservableProperty, Object): boolean}
+ */
+sb.binding.Propagation;

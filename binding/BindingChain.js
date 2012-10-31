015a9f5f55799274b47c214650e14485cf3d2e3e
Takuya Ueda <t.ueda@netcom-inc.co.jp>
2012-10-31 17:18:41 +0900

    change namespace

---
+++ binding/BindingChain.js
@@ -0,0 +1,192 @@
+(function(){
+
+    /**
+     * A set of bindings which provide binding functions as method chains.
+     * @param {sb.binding.Observer} observer 
+     * @param {Array.<sb.observable.ObservableObject>} observables
+     */
+    sb.binding.BindingChain = function(observer, observables) {
+
+        /**
+         * Create bindings.
+         * @type {sb.util.expandable}
+         */
+        var bindingsMaker = sb.util.expandable();
+
+        /**
+         * A set of bindings.
+         * @type {Array.<sb.binding.Binding>}
+         */
+        var bindings = [];
+
+        /**
+         * Synchronize observables which are same value each other
+         * and when an observable changes, others immediately synchronized.
+         * 
+         * @return {sb.binding.BindingChain} own 
+         */
+        this.synchronize = function() {
+
+            /**
+             * Arguments array this function.
+             * @type {Array.<*>}
+             */
+            var args = sb.util.argumentsToArray(arguments);
+
+            /**
+             * Observables which are synchronized.
+             * @type {Array.<sb.binding.ObservableObject>}
+             */
+            var syncObservables = [];
+
+            args.forEach(function(arg) {
+                if (sb.observable.isObservable(arg)) {
+                    syncObservables.push(arg);
+                    if (observables.indexOf(arg) < 0) {
+                        observables.push(arg);
+                    }
+                } 
+            }); 
+
+            /**
+             * Bindings which are used for synchronize given observables.
+             * @type {Array.<sb.binding.Binding>}
+             */
+            var syncBindings = syncObservables.map(function(input) {
+                var inputs = {input: input};
+                var outputs = {};
+                syncObservables.forEach(function(output, i) {
+                    if (input !== output) {
+                        outputs["output"+i] = output;
+                    }
+                });
+                var computed = function(inputs) { 
+                    var results = {};
+                    Object.keys(outputs).forEach(function(key){
+                        results[key] = inputs.input();
+                    });
+                    return results;
+                };
+                var b = new sb.binding.Binding(observer, inputs, outputs, computed);
+                return b;
+            });
+
+
+            // for lazy evaluation
+            bindingsMaker.expand(function() {
+                bindings = bindings.concat(syncBindings);
+            });
+
+            return this;
+        };
+
+        
+        /**
+         * Add computed binding.
+         * @param {sb.observable.ObservableObject} observable target observable
+         * @param {sb.binding.Computed} func computed function
+         * @return {sb.binding.BindingChain} own
+         */
+        this.computed = function(observable, func) {
+
+            if (!sb.observable.isObservable(observable)
+                    || typeof func !== "function") {
+                return this;
+            }
+
+            if (observables.indexOf(observable) < 0) {
+                observables.push(observable);
+            }
+
+            // lazy evaluation
+            bindingsMaker.expand(function() {
+                var inputs = {};
+                observables.forEach(function(input, i) {
+                    if (observable !== input) {
+                        inputs["input"+i] = input;
+                    }
+                });
+                var outputs = {output: observable}; 
+
+                var b = new sb.binding.Binding(
+                    observer,
+                    inputs,
+                    outputs,
+                    function(inputs) {
+                        return {output: func(inputs)};
+                    }
+                );
+
+                bindings.push(b);
+            });
+
+            return this;
+        };
+
+        /**
+         * Add callback which call after changing a given observable value.
+         * @param {sb.observable.ObservableObject} observable target observable.
+         * @param {function(*):*} callback callback function
+         * @return {sb.binding.BindingChain} own
+         */
+        this.onChange = function(observable, callback) {
+
+            if (!sb.observable.isObservable(observable)
+                    || typeof callback !== "function") {
+                return this;
+            }
+
+            if (observables.indexOf(observable)) {
+                observables.push(observable);
+            }
+
+            var b = new sb.binding.Binding(
+                observer,
+                {input: observable},
+                {},
+                function() {
+                    callback();
+                    return {};
+                }
+            );
+
+            // for lazy evaluation
+            bindingsMaker.expand(function() {
+                bindings.push(b);
+            });
+            return this;
+        };
+
+        /**
+         * Enable internal all internal bindings.
+         * @return {sb.binding.BindingChain} own
+         */
+        this.bind = function() {
+
+            // init
+            this.unbind();
+            bindingsMaker();
+
+            bindings.forEach(function(b) {
+                b.bind();
+            });
+
+            return this;
+        };
+
+        /**
+         * Disable internal all internal bindings.
+         * @return {sb.binding.BindingChain} own
+         */
+        this.unbind = function() {
+            
+            bindings.forEach(function(b) {
+                b.unbind();
+            });
+            bindings = [];
+
+            return this;
+        };
+    };
+
+})();

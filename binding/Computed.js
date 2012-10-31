885d502eee48626e0f17fc57240064640ecb3852
Takuya Ueda <t.ueda@netcom-inc.co.jp>
2012-10-31 18:01:47 +0900

    change namespace

---
+++ binding/Computed.js
@@ -0,0 +1,7 @@
+/**
+ * Computed function.
+ * It is used when notifies changing an observable value
+ * to other binded observables by using computing notified value at sb.binding.Binding.
+ * @typedef {function(sb.binding.Parameters):sb.binding.Parameters}
+ */
+sb.binding.Computed;

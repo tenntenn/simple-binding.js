f9cab8dc5c3a88449474f0cbadd80c95dfc78753
tenntenn <uedatakuya@gmail.com>
2012-11-07 10:01:39 +0900

    fixed for requirejs

--- src/util/main.js
+++ src/util/main.js
@@ -1,5 +1,19 @@
-/**
- * It provides utility functions and data structures.
- * @namespace
- */
-sb.util = sb.util || {};
+define(
+        "sb/util",
+        [
+                "sb/util/argumentsToArray.js",
+                "sb/util/expandable.js"
+        ],
+        function(argumentsToArray, expandable) {
+                /**
+                 * It provides utility functions and data structures.
+                 * @namespace
+                 */
+                var util = {
+                        argumentsToArray: argumentsToArray,
+                        expandable: expandable
+                };
+
+                return util;
+        }
+);

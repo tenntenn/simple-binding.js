937ccd6c827ec2a125b541d4e06dad2e1fe9b1a4
spd-user <katsumata.yuji@gmail.com>
2012-11-21 02:15:28 +0900

    add simplest tests using mocha-phantomjs

---
+++ test/testSB.js
@@ -0,0 +1,14 @@
+// test.js
+
+describe("simple binding test", function(){
+    it('get test', function(){
+	var hoge = sb.observable(100);
+	expect(100).to.equal(hoge());
+    });
+
+    it('set test', function(){
+	var hoge = sb.observable(100);
+	hoge(200);
+	expect(200).to.equal(hoge());
+    });
+});
